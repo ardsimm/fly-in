ef14c04ae316dbd67114b9ca1226bbe94143e8b2
--global <smenard@42lyon.fr>
2026-09-17 17:15:05 +0200

    feat: basic bfs implementation

---
+++ src/simulation/__init__.py
@@ -0,0 +1,8 @@
+from .simulation import Simulation
+from .simulation_exceptions import SimulationException, PathNotFoundError
+
+__all__ = [
+    "PathNotFoundError",
+    "Simulation",
+    "SimulationException"
+]
